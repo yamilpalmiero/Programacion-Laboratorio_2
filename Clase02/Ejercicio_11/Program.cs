d2649557979da17336e99aa7886fec773b943582
Yamil <yamilpalmiero@live.com>
2021-03-20 11:02:16 -0300

    Se empieza con ejercicios

---
+++ Clase02/Ejercicio_11/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Ejercicio_11.Validacion;
+
+namespace Ejercicio_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            int acumulador = 0;
+
+            for(int i=0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese numero entero entre -100 y 100:");
+                valor = int.Parse(Console.ReadLine());
+
+                while(!Validacion.Validar(valor, -100, 100))
+                {
+                    Console.WriteLine("Error, el numero debe ser entre -100 y 100. Reingrese:");
+                    valor = int.Parse(Console.ReadLine());
+                }
+
+                if (valor > max)
+                {
+                    max = valor;
+                }
+                if (valor < min)
+                {
+                    min = valor;
+                }
+
+                acumulador += valor;
+            }
+
+            Console.WriteLine("El minimo es {0}", min);
+            Console.WriteLine("El maximo es {0}", max);
+            Console.WriteLine("El promedio es {0}", acumulador/10);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_01/Ejercicio_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            int acumulador = 0;
+
+            Console.WriteLine("Ingrese cinco numeros:");
+            
+            for(int i=0; i < 5; i++)
+            {
+                Console.Write("i° numero:");
+                numero = int.Parse(Console.Read());
+
+            }
+        }
+    }
+}
