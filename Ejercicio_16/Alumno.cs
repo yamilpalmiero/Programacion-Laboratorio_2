406461f639fc70ace2dbd51d762560f663c4ef83
Yamil <yamilpalmiero@live.com>
2021-03-25 21:23:59 -0300

    Ejercicio 16 terminado

---
+++ Ejercicio_16/Alumno.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Alumno
+    {
+        byte nota1;
+        byte nota2;
+        float notaFinal;
+        string apellido;
+        int legajo;
+        string nombre;
+
+        public Alumno(string nom, string ape, int leg)
+        {
+            this.nombre = nom;
+            this.apellido = ape;
+            this.legajo = leg;
+        }
+
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            this.nota1 = notaUno;
+            this.nota2 = notaDos;
+        }
+
+        public void CalcularFinal()
+        {
+            Random notaRandom = new Random();
+
+            if (this.nota1 >= 4 && this.nota2 >= 4)
+            {
+                this.notaFinal = notaRandom.Next(6, 10);
+            }
+            else
+            {
+                this.notaFinal = -1;
+            }
+        }
+
+        public string Mostrar()
+        {
+            if (this.notaFinal == -1)
+            {
+                return "Nombre: " + this.nombre + "\nApellido: " + this.apellido + "\nLegajo: " + this.legajo + "\nNota 1: " + this.nota1 + "\nNota 2: " + this.nota2 + "\nNota final: Alumno desaprobado\n\n";
+            }
+            else
+            {
+                return "Nombre: " + this.nombre + "\nApellido: " + this.apellido + "\nLegajo: " + this.legajo + "\nNota 1: " + this.nota1 + "\nNota 2: " + this.nota2 + "\nNota final: " + notaFinal + "\n\n";
+            }
+        }
+
+    }
+}
---
+++ Ejercicio_16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_16";
+
+            Alumno alumno1 = new Alumno("Yamil", "Palmiero", 100);
+            Alumno alumno2 = new Alumno("Mariana", "Seib", 200);
+            Alumno alumno3 = new Alumno("Maria", "Martu", 300);
+
+            alumno1.Estudiar(2, 8);
+            alumno2.Estudiar(6, 5);
+            alumno3.Estudiar(9, 8);
+
+            alumno1.CalcularFinal();
+            alumno2.CalcularFinal();
+            alumno3.CalcularFinal();
+
+            Console.WriteLine(alumno1.Mostrar());
+            Console.WriteLine(alumno2.Mostrar());
+            Console.WriteLine(alumno3.Mostrar());
+
+            Console.ReadKey();
+        }
+    }
+}
