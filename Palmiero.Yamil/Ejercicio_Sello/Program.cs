0d05013f1355d0f54e3deb2622e4fe3c74d0271a
Yamil <yamilpalmiero@live.com>
2021-03-31 11:44:57 -0300

    Ejercicio_Sello

---
+++ Palmiero.Yamil/Ejercicio_Sello/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Sello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_Sello";
+
+            string cadena; //Para guardar lo que devuelve Imprimir
+
+            Sello.mensaje = "Primer mensaje";
+            cadena = Sello.Imprimir(); //Porque Imprimir devuelve una cadena
+            Console.WriteLine("El mensaje es: {0}", cadena); //IMPRIMO PRIMER MENSAJE NORMAL
+
+            Sello.Borrar();
+            cadena = Sello.Imprimir();
+            Console.WriteLine("El mensaje es: {0}", cadena); //BORRO PRIMER MENSAJE E IMPRIMO VACIO
+
+            Sello.mensaje = "Segundo mensaje";
+            Sello.color = ConsoleColor.Yellow;
+            Sello.ImprimirColor(); //IMPRIMO SEGUNDO MENSAJE EN COLOR
+            cadena = Sello.Imprimir();
+            Console.WriteLine("El mensaje es: {0}", cadena); //IMPRIMO SEGUNDO MENSAJE EN GRIS (DEFAULT)
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Palmiero.Yamil/Ejercicio_Sello/Sello.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Sello
+{
+    class Sello
+    {
+        public static string mensaje;
+        public static ConsoleColor color;
+
+        public static string Imprimir()
+        {
+            return Sello.mensaje;
+        }
+
+        public static void Borrar()
+        {
+            Sello.mensaje = "";
+        }
+
+        public static void ImprimirColor()
+        {
+            string cad;
+
+            cad = Sello.Imprimir();
+            Console.ForegroundColor = Sello.color;
+            Console.WriteLine("El mensaje es: {0}", Sello.mensaje);
+            Sello.color = ConsoleColor.Gray;
+            Console.ForegroundColor = Sello.color;
+        }
+    }
+}
