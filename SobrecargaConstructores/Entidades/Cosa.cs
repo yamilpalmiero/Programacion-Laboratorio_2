e8d7717a34604d7eb99abb4ec53e617b586bb96c
Yamil <yamilpalmiero@live.com>
2021-04-03 21:33:02 -0300

    Sobrecarga de constructores y Biblioteca de Clases

---
+++ SobrecargaConstructores/Entidades/Cosa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cosa
+    {
+        #region Atributos Estaticos
+        private int entero;
+        private string cadena;
+        private DateTime fecha;
+        #endregion
+
+        #region Constructores de Instancia
+        public Cosa()
+        {
+            this.entero = 0;
+            this.cadena = "Hola Mundo";
+            this.fecha = DateTime.Now;
+        }
+
+        public Cosa(string cad) : this()//Se invoca al constructor que no recibe nada para que inicialice los otros dos datos
+        {
+            //this.entero = 0; Se repite en el por defecto
+            this.cadena = cad;
+            //this.fecha = DateTime.Now; Se repite en el por defecto
+        }
+
+        public Cosa(int numero, string cad) : this(cad)
+        {
+            this.entero = numero;
+            //this.cadena = cad;
+            //this.fecha = DateTime.Now;
+        }
+        #endregion
+
+        #region Metodos
+        public void Mostrar()
+        {
+            Console.WriteLine("\nValor del entero: {0}\nString: {1}\nFecha: {2}\n", this.entero, this.cadena, this.fecha);
+        }
+        #endregion
+    }
+}
---
+++ SobrecargaConstructores/SobrecargaConstructores/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaConstructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Sobrecarga de Constructores";
+
+            Cosa objeto1 = new Cosa();
+            objeto1.Mostrar();
+
+            Cosa objeto2 = new Cosa("Hola");
+            objeto2.Mostrar();
+
+            Cosa objeto3 = new Cosa(5, "Chau");
+            objeto3.Mostrar();
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
