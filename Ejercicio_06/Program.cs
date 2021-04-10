0f3256fe5ba3a60081c0e6ed0a29483b64ca633d
Yamil <yamilpalmiero@live.com>
2021-04-09 22:35:43 -0300

    Corregido

--- Ejercicio_06/Program.cs
+++ Ejercicio_06/Program.cs
@@ -12,32 +12,41 @@ namespace Ejercicio_06
         {
             Console.Title = "Ejercicio_06";
 
-            int anioInicio, anioFin;
+            int desde = pedirPositivo("Ingrese el anio de inicio: ", "Error, reingrese.");
+            int hasta = pedirPositivo("Hasta el año: ", "Error, reingrese.");
 
-            Console.WriteLine("Ingrese anio de inicio:");
-            anioInicio = int.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese anio de finalizacion:");
-            anioFin = int.Parse(Console.ReadLine());
-
-            for(int i=anioInicio; i<=anioFin; i++)
+            Console.WriteLine("\nAños bisiestos: ");
+            for (int i = desde; i <= hasta; i++)
             {
-                if (i % 4 == 0 && i < 100)
+                if ((i % 4 == 0) && (i % 100 != 0 || i % 400 == 0))
                 {
-                    Console.WriteLine("{0} es bisiesto.", i);
-                }
-                if(i % 4 == 0 && i >= 100)
-                {
-                    if(i % 100 == 0 && !(i % 400 == 0))
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        Console.WriteLine("{0} es bisiesto.", i);
-                    }
+                    Console.WriteLine(i);
                 }
             }
 
+            #region Metodos
+            int pedirPositivo(string mensaje, string mensajeError)
+            {
+                int numero;
+                bool error;
+
+                Console.WriteLine(mensaje);
+
+                do
+                {
+                    if (!int.TryParse(Console.ReadLine(), out numero) || numero <= 0)
+                    {
+                        Console.WriteLine(mensajeError);
+                        error = true;
+                    }
+                    else
+                        error = false;
+
+                } while (error);
+
+                return numero;
+            }
+            #endregion
             Console.ReadKey();
         }
     }
--- Ejercicio_07/Program.cs
+++ Ejercicio_07/Program.cs
@@ -12,15 +12,77 @@ namespace Ejercicio_07
         {
             Console.Title = "Ejercicio_07";
 
-            DateTime fechaNac;
-            DateTime fechaHoy = DateTime.Today;
-            TimeSpan cantDias;
+            int anio = pedirEnteroPositivo("Año de nacimiento: ", "Error, reingrese: ");
+            int mes = pedirEnteroEnRango(1, 12, "Mes: ", "Error, debe ser del 1 al 12: ");
+            int dia = pedirDia(mes);
 
-            Console.WriteLine("Fecha de nacimiento (dd/mm/aaaa): ");
-            fechaNac = DateTime.Parse(Console.ReadLine());
-            cantDias = DateTime.Today.Subtract(fechaNac);
+            int cantDias = ((TimeSpan)(DateTime.Now - new DateTime(anio, mes, dia))).Days;
 
-            Console.WriteLine("Cantidad de dias vividos: " + cantDias.TotalDays);
+            if (cantDias > 0)
+                Console.WriteLine("Pasaron {0} dias desde el nacimiento.", cantDias);
+            else
+                Console.WriteLine("Todavia no nacio.");
+
+            Console.ReadKey(true);
+
+            #region Metodos
+            int pedirEnteroPositivo(string mensaje, string mensajeError)
+            {
+                int numero;
+                bool error;
+
+                Console.WriteLine(mensaje);
+
+                do
+                {
+                    if (!int.TryParse(Console.ReadLine(), out numero) || numero <= 0)
+                    {
+                        Console.WriteLine(mensajeError);
+                        error = true;
+                    }
+                    else
+                        error = false;
+                } while (error);
+
+                return numero;
+            }
+
+            int pedirEnteroEnRango(int desde, int hasta, string mensaje, string mensajeError)
+            {
+                int numero;
+                bool error;
+
+                Console.WriteLine(mensaje);
+
+                do
+                {
+                    if (!int.TryParse(Console.ReadLine(), out numero) || numero <= 0 || numero < desde || numero > hasta)
+                    {
+                        Console.WriteLine(mensajeError);
+                        error = true;
+                    }
+                    else
+                        error = false;
+                } while (error);
+
+                return numero;
+            }
+
+            int pedirDia(int m)
+            {
+                int[] mesesDeTreinta = { 4, 6, 9, 11 };
+                int diasEnElMes;
+
+                if (mes == 2)
+                    diasEnElMes = 28;
+                else if (mesesDeTreinta.Contains(m))
+                    diasEnElMes = 30;
+                else
+                    diasEnElMes = 31;
+
+                return pedirEnteroEnRango(1, diasEnElMes, "Dia de nacimiento: ", "Error, reingrese: ");
+            }
+            #endregion
 
             Console.ReadKey();
         }
