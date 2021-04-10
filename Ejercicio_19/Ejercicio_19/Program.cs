62384dce2ff9de1b0ee34f4581e465c1dc77d07f
Yamil <yamilpalmiero@live.com>
2021-04-10 19:08:39 -0300

    Terminado, falta probar

---
+++ Ejercicio_19/Ejercicio_19/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador a = new Sumador(1);
+            a.Sumar(2, 3);
+            a.Sumar("Hola", "Mundo");
+
+            Sumador b = new Sumador();
+            b.Sumar(4, 5);
+            b.Sumar("a", "b");
+
+            int c = (int)a;
+
+            bool d = a | b;
+            b.Sumar(2, 3);
+            bool e = a | b;
+            long f = a + b;
+            long g = f;
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Ejercicio_19/Ejercicio_19/Sumador.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_19
+{
+    class Sumador
+    {
+        #region Atributos
+        private int cantidadSumas;
+        #endregion
+
+        #region Constructores
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+        public Sumador() : this(0)
+        {
+
+        }
+        #endregion
+
+        #region Metodos
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+        public string Sumar(string a, string b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        public static explicit operator int(Sumador s)
+        {
+            return s.cantidadSumas;
+        }
+
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas + s2.cantidadSumas;
+        }
+
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas == s2.cantidadSumas;
+        }
+        #endregion
+
+    }
+}
