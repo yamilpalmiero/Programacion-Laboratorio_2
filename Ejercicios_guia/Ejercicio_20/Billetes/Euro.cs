b6abc3dac508cee578c053c0fd901e9eeab3d7b6
Yamil <yamilpalmiero@live.com>
2021-04-11 03:00:47 -0300

    Falta Euro y Pesos

---
+++ Ejercicios_guia/Ejercicio_20/Billetes/Euro.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billetes
+{
+    public class Euro
+    {
+        #region Atributos
+        private double cantidad;
+        private static double cotizRespectoDolar;
+        #endregion
+
+        #region Sobrecarga Constructores
+        private Euro()
+        {
+            Euro.cotizRespectoDolar = 1.08;
+        }
+        public Euro(double cantidad) : this()
+        {
+            this.cantidad = cantidad;
+        }
+        public Euro(double cantidad, double cotizacion) : this(cantidad)
+        {
+            Euro.cotizRespectoDolar = cotizacion;
+        }
+        #endregion
+
+        #region Getters
+        public double GetCantidad()
+        {
+            return this.cantidad;
+        }
+        public static double GetCotizacion()
+        {
+            return Euro.cotizRespectoDolar;
+        }
+        #endregion
+
+        #region Sobrecarga Exp Imp
+        public static explicit operator Dolar(Euro e)
+        {
+            return new Dolar(e.cantidad * Euro.GetCotizacion());
+        }
+        public static explicit operator Pesos(Euro e)
+        {
+            return new Pesos(e.cantidad * Euro.GetCotizacion() / Pesos.GetCotizacion());
+        }
+        public static implicit operator Euro(double d)
+        {
+            return new Euro(d);
+        }
+        #endregion
+
+        #region Sobrecarga Operadores Comparacion
+        public static bool operator ==(Euro e1, Euro e2)
+        {
+            return e1.GetCantidad() == e2.GetCantidad();
+        }
+        public static bool operator !=(Euro e1, Euro e2)
+        {
+            return !(e1 == e2);
+        }
+
+        public static bool operator ==(Euro e, Pesos p)
+        {
+            return (Pesos)e == p;
+        }
+        public static bool operator !=(Euro e, Pesos p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator ==(Euro e, Dolar d)
+        {
+            return (Dolar)e == d;
+        }
+        public static bool operator !=(Euro e, Dolar d)
+        {
+            return !(e == d);
+        }
+        #endregion
+
+        #region Sobrecarga Operadores Aritmeticos
+        public static Euro operator +(Euro e, Dolar d)
+        {
+            return e + d;
+        }
+        public static Euro operator +(Euro e, Pesos p)
+        {
+            return e + p;
+        }
+
+        public static Euro operator -(Euro e, Dolar d)
+        {
+            return e - d;
+        }
+        public static Euro operator -(Euro e, Pesos p)
+        {
+            return e - p;
+        }
+        #endregion
+    }
+}
---
+++ Ejercicios_guia/Ejercicio_20/Ejercicio_20/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Billetes;
+
+namespace Ejercicio_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolar dolar = new Dolar(100);
+            Euro euro = new Euro(100);
+            Pesos peso = new Pesos(660);
+            Dolar total = dolar + euro + peso;
+            Console.WriteLine(total.cantidad);
+
+            if (total.GetCantidad() > 99 && total.GetCantidad() < 101)
+            {
+                Console.WriteLine(total.GetCantidad());
+                Console.WriteLine("OK");
+            }
+            else
+                Console.WriteLine("NO");
+
+            if ((Pesos)new Dolar(100) == new Pesos(660))
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("NO");
+
+            Console.ReadKey();
+        }
+    }
+}
