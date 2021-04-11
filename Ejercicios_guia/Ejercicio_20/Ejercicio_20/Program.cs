9d38365470576cb9a831c2df5a70c93b7d49b1ea
Yamil <yamilpalmiero@live.com>
2021-04-11 12:13:58 -0300

    Tiene errores de casteo

--- Ejercicios_guia/Ejercicio_20/Ejercicio_20/Program.cs
+++ Ejercicios_guia/Ejercicio_20/Ejercicio_20/Program.cs
@@ -13,23 +13,27 @@ namespace Ejercicio_20
         static void Main(string[] args)
         {
             Dolar dolar = new Dolar(100);
-            Euro euro = new Euro(100);
-            Pesos peso = new Pesos(660);
+            Euro euro = new Euro(92.5925926);
+            Pesos peso = new Pesos(6600);
             Dolar total = dolar + euro + peso;
-            Console.WriteLine(total.cantidad);
+            Console.WriteLine(total.GetCantidad());
 
-            if (total.GetCantidad() > 99 && total.GetCantidad() < 101)
-            {
-                Console.WriteLine(total.GetCantidad());
-                Console.WriteLine("OK");
-            }
-            else
-                Console.WriteLine("NO");
 
-            if ((Pesos)new Dolar(100) == new Pesos(660))
-                Console.WriteLine("OK");
+            if ((Pesos)new Dolar(100) == new Pesos(6600))
+                Console.WriteLine("Explicito Pesos = OK");
             else
-                Console.WriteLine("NO");
+                Console.WriteLine("Explicito Pesos = NO");
+
+            if (new Dolar(100) == (Dolar)new Pesos(6600))
+                Console.WriteLine("Explicito Dolar = OK");
+            else
+                Console.WriteLine("Explicito Dolar = NO");
+
+            if ((Euro)new Dolar(108) == new Euro(100))
+                Console.WriteLine("Explicito Euro = OK");
+            else
+                Console.WriteLine("Explicito Euro = NO");
+
 
             Console.ReadKey();
         }
