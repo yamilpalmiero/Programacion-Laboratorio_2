7c6a7961072cb2d9432e0fbc3fa6e65fca9fe408
Yamil <yamilpalmiero@live.com>
2021-04-11 21:01:52 -0300

    Se empezo con ejercicio integrador

---
+++ Clases/Clase_05_EjercicioIntegrador/Repaso/Clases/Producto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigo;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        public static string MostrarProducto(Producto p)
+        {
+            return 
+        }
+    }
+}
