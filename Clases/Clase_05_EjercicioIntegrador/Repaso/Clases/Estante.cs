570cf2bfe27ea22cc693c6b56fb51ee178890115
Yamil <yamilpalmiero@live.com>
2021-04-12 22:26:33 -0300

    Terminado

--- Clases/Clase_05_EjercicioIntegrador/Repaso/Clases/Estante.cs
+++ Clases/Clase_05_EjercicioIntegrador/Repaso/Clases/Estante.cs
@@ -8,5 +8,90 @@ namespace Clases
 {
     public class Estante
     {
+        #region Atributos
+        private Producto[] productos;
+        private int ubicacionEstante;
+        #endregion
+
+        #region Constructores
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+        #endregion
+
+        #region Metodos
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Ubicacion: {0}", e.ubicacionEstante);
+
+            foreach (Producto p in e.productos)
+            {
+                if (!Object.ReferenceEquals(p, null))// if (!(p is null)) es lo mismo
+                    sb.AppendLine(Producto.MostrarProducto(p));
+            }
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Sobrecarga de Operadores
+        public static bool operator ==(Estante e, Producto p)
+        {
+            return e.GetProductos().Contains<Producto>(p);//Si el Estante (clase) e contiene el Producto p devuelve true
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+        public static bool operator +(Estante e, Producto p)
+        {
+            int i = 0;
+
+            if (e != p)//Por la sobrecarga de != esto significa que en ese estante no se encuentra ese producto
+            {
+                foreach (Producto producto in e.GetProductos())
+                {
+                    if (producto is null)//Osea que este espacio esta vacio
+                    {
+                        e.GetProductos()[i] = p;
+                        return true;
+                    }
+                    i++;
+                }
+            }
+
+            return false;
+        }
+        public static Estante operator -(Estante e, Producto p)
+        {
+            int i = 0;
+
+            if (e == p)//Si el estante contiene al producto
+            {
+                foreach (Producto producto in e.GetProductos())
+                {
+                    if (producto == p)
+                    {
+                        e.GetProductos()[i] = null;
+                        break;
+                    }
+                    i++;
+                }
+            }
+
+            return e;
+        }
+        #endregion
     }
 }
--- Clases/Clase_05_EjercicioIntegrador/Repaso/Clases/Producto.cs
+++ Clases/Clase_05_EjercicioIntegrador/Repaso/Clases/Producto.cs
@@ -8,17 +8,22 @@ namespace Clases
 {
     public class Producto
     {
+        #region Atributos
         private string codigoDeBarra;
         private string marca;
         private float precio;
+        #endregion
 
+        #region Constructor
         public Producto(string marca, string codigo, float precio)
         {
             this.marca = marca;
             this.codigoDeBarra = codigo;
             this.precio = precio;
         }
+        #endregion
 
+        #region Metodos
         public string GetMarca()
         {
             return this.marca;
@@ -29,7 +34,33 @@ namespace Clases
         }
         public static string MostrarProducto(Producto p)
         {
-            return 
+            return string.Format("Codigo de barras: {0}\nMarca: {1}\nPrecio: {2}", p.codigoDeBarra, p.marca, p.precio);
         }
+        #endregion
+
+        #region Sobrecarga de Operadores
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            if (p1.codigoDeBarra == p2.codigoDeBarra && p1.marca == p2.marca)
+                return true;
+            return false;
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+        public static bool operator ==(Producto p, string marca)
+        {
+            return p.marca == marca;
+        }
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+        #endregion
     }
 }
