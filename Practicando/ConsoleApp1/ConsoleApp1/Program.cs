72de844bc3954b78ad8ac56e8f916554cdcc0d31
Yamil <yamilpalmiero@live.com>
2021-04-30 21:12:34 -0300

    Foreach y salida por consola

---
+++ Practicando/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado = 0;
+            string[] nombres = new string[5];
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Title = "Mi Proyecto";
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.Write("{0} nombre: ", i + 1);
+                nombres[i] = Console.ReadLine();
+            }
+
+            foreach (string auxNombre in nombres)
+            {
+                Console.WriteLine(auxNombre);
+            }
+
+
+            DateTime dt = DateTime.Now;
+
+            resultado = suma(DateTime.Now.Minute, DateTime.Now.Second);
+
+            Console.WriteLine("La suma de {0} minutos con {1} segundos es: {2}", dt.Minute, dt.Second, resultado);
+
+            Console.ReadKey(true);
+
+        }
+
+        static int suma(int numero1, int numero2)
+        {
+            if (numero1 > 25 || numero2 > 25)
+            {
+                return numero1 + numero2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
