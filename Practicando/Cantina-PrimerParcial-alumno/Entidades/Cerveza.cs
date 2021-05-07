3dde8834b8d2c988c1738028d5e44a5a08e588fe
Yamil <yamilpalmiero@live.com>
2021-05-06 21:52:54 -0300

    Se empezo con el parcial de cantina

---
+++ Practicando/Cantina-PrimerParcial-alumno/Entidades/Cerveza.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cerveza : Botella
+    {
+        #region Atributos
+        private const int MEDIDA = 330;
+        private Tipo tipo;
+        #endregion
+
+        #region Constructores
+        public Cerveza(int capacidadML, string marca, int contenidoML) :base(marca, capacidadML, contenidoML)
+        {
+            this.tipo = tipo;
+        }
+        public Cerveza(int capacidadML, string marca, Tipo tipo, int contenido)
+        #endregion
+    }
+}
