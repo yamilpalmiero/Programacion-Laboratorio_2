3ad190b7ff9818e32c4b227dbe03be3725a48609
Yamil Palmiero <yamilpalmiero@live.com>
2021-06-16 20:42:10 -0300

    Terminado

---
+++ Ejercicios_guia/Ejercicio_38/Ejercicio_38/Sobreescrito.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_38
+{
+    public class Sobreescrito
+    {
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Este es mi étodo ToString sobreescrito!");
+            return sb.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Sobreescrito;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+    }
+}
