c566c2fa37f50424a1c2d701bef137346f96ea89
Yamil Palmiero <yamilpalmiero@live.com>
2021-06-20 19:19:25 -0300

    Falta terminar

---
+++ Ejercicios_guia/Ejercicio_42/Clases/MiClase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class MiClase
+    {
+        private float a;
+        private float b;
+        private char operador;
+
+        public MiClase()
+        {
+            try
+            {
+                MiClase.Calcular();
+            }
+            catch (DivideByZeroException e)
+            {
+                throw new DivideByZeroException("Mensaje DivideByZero", e);
+            }
+        }
+        public MiClase(float numero1, float numero2, char operador)
+            : this()
+        {
+            this.a = numero1;
+            this.b = numero2;
+            this.operador = operador;
+
+            try
+            {
+
+            }
+        }
+
+        private static float Calcular()
+        {
+            throw new DivideByZeroException();
+        }
+    }
+}
