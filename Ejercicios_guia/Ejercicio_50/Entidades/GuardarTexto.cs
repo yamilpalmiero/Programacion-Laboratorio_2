140d56abc5d9c23aedd2a8426ff6f49a83961dd5
Yamil Palmiero <yamilpalmiero@live.com>
2021-06-23 19:54:48 -0300

    50 terminado

---
+++ Ejercicios_guia/Ejercicio_50/Entidades/GuardarTexto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class GuardarTexto<T, V> : IGuardar<T, V>
+    {
+        public bool Guardar(T obj)
+        {
+            return true;
+        }
+
+        public V Leer()
+        {
+            return (V)Convert.ChangeType("Texto Leído", typeof(V));
+        }
+    }
+}
