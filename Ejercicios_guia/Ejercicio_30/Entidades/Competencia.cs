714a0e0d0c26423147a4d455c52fc390b9cbc9fd
Yamil Palmiero <yamilpalmiero@live.com>
2021-06-23 19:00:19 -0300

    49 sin terminar

--- Ejercicios_guia/Ejercicio_30/Entidades/Competencia.cs
+++ Ejercicios_guia/Ejercicio_30/Entidades/Competencia.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public class Competencia
+    public class Competencia<T> where T : VehiculoDeCarrera
     {
         public enum TipoCompetencia
         {
