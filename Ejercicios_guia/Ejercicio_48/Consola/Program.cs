7501c2dd1c85a882daf46d33b3d6d72e147bce2f
Yamil Palmiero <yamilpalmiero@live.com>
2021-06-23 18:51:27 -0300

    Terminado

---
+++ Ejercicios_guia/Ejercicio_48/Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidades;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contabilidad<Factura, Recibo> c = new Contabilidad<Factura, Recibo>();
+
+            c += new Recibo(2);
+            c += new Factura(3);
+            c += new Recibo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicios_guia/Ejercicio_48/Entidades/Recibo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Recibo : Documento
+    {
+        public Recibo(int numero)
+            : base(numero)
+        {
+
+        }
+        public Recibo()
+            : this(0)
+        {
+
+        }
+    }
+}
